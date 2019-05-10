3ca5b1f6959c507ff0e8cfb100fb7070063ea19d
Manuel <viger.manuel@gmail.com>
2019-05-09 23:33:06 -0400

    Interface terminée

--- ProjetTp2/src/packageTP2/AppSuperCheapAuto.java
+++ ProjetTp2/src/packageTP2/AppSuperCheapAuto.java
@@ -5,12 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JRadioButton;
 
 public class AppSuperCheapAuto extends JFrame {
+	private DefaultTableModel modele;
+	
 	private JPanel panelClient;
 	private JLabel jlNumMembre;
 	private JLabel jlNomClient;
@@ -21,6 +30,24 @@ public class AppSuperCheapAuto extends JFrame {
 	private JPanel panelCommande;
 	private JLabel jlCommande;
 	private JLabel jlArticle;
+	private JLabel jlPrixUni;
+	private JLabel jlQte;
+	private JTextField tfPrixUni;
+	private JTextField tfQte;
+	private JComboBox jcBoxArticle;
+	private JButton btnAchat;
+	private JButton btnTerminer;
+	private JPanel panelFacture;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JRadioButton btnRadioComptant;
+	private JRadioButton btnRadioCredit;
+	private JButton btnPayer;
+	private JLabel labelMontantDonne;
+	private JLabel labelMontantRemis;
+	private JTextField tfMontantDonne;
+	private JTextField tfMontantRemis;
+	private JButton btnAnnuNouvComm;
 
 	/**
 	 * Launch the application.
@@ -87,7 +114,7 @@ public class AppSuperCheapAuto extends JFrame {
 		panelCommande = new JPanel();
 		panelCommande.setBackground(Color.ORANGE);
 		panelCommande.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		panelCommande.setBounds(10, 114, 304, 92);
+		panelCommande.setBounds(10, 114, 304, 114);
 		getContentPane().add(panelCommande);
 		panelCommande.setLayout(null);
 		
@@ -98,8 +125,104 @@ public class AppSuperCheapAuto extends JFrame {
 		
 		jlArticle = new JLabel("Article");
 		jlArticle.setFont(new Font("Dialog", Font.BOLD, 12));
-		jlArticle.setBounds(10, 33, 85, 14);
+		jlArticle.setBounds(10, 33, 58, 14);
 		panelCommande.add(jlArticle);
+		
+		jlPrixUni = new JLabel("Prix unitaire:");
+		jlPrixUni.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		jlPrixUni.setBounds(10, 59, 101, 16);
+		panelCommande.add(jlPrixUni);
+		
+		jlQte = new JLabel("Qté en Stock:");
+		jlQte.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		jlQte.setBounds(197, 59, 101, 16);
+		panelCommande.add(jlQte);
+		
+		tfPrixUni = new JTextField();
+		tfPrixUni.setBounds(6, 82, 85, 26);
+		panelCommande.add(tfPrixUni);
+		tfPrixUni.setColumns(10);
+		
+		tfQte = new JTextField();
+		tfQte.setColumns(10);
+		tfQte.setBounds(207, 82, 85, 26);
+		panelCommande.add(tfQte);
+		
+		jcBoxArticle = new JComboBox();
+		jcBoxArticle.setBounds(71, 28, 221, 27);
+		panelCommande.add(jcBoxArticle);
+		
+		btnAchat = new JButton("Achat");
+		btnAchat.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnAchat.setBounds(324, 115, 147, 57);
+		getContentPane().add(btnAchat);
+		
+		btnTerminer = new JButton("Terminer");
+		btnTerminer.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnTerminer.setBounds(326, 171, 147, 57);
+		getContentPane().add(btnTerminer);
+		
+		panelFacture = new JPanel();
+		panelFacture.setBackground(Color.RED);
+		panelFacture.setBounds(10, 240, 461, 266);
+		getContentPane().add(panelFacture);
+		panelFacture.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 449, 114);
+		panelFacture.add(scrollPane);
+		
+		modele = new DefaultTableModel();
+		table = new JTable(modele);
+		scrollPane.setViewportView(table);
+		
+		btnRadioComptant = new JRadioButton("Paiement comptant");
+		btnRadioComptant.setForeground(Color.WHITE);
+		btnRadioComptant.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnRadioComptant.setBounds(6, 145, 163, 23);
+		panelFacture.add(btnRadioComptant);
+		
+		btnRadioCredit = new JRadioButton("Paiement crédit");
+		btnRadioCredit.setForeground(Color.WHITE);
+		btnRadioCredit.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnRadioCredit.setBounds(181, 145, 138, 23);
+		panelFacture.add(btnRadioCredit);
+		
+		btnPayer = new JButton("Payer");
+		btnPayer.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnPayer.setBounds(322, 132, 133, 68);
+		panelFacture.add(btnPayer);
+		
+		labelMontantDonne = new JLabel("Montant donnée:");
+		labelMontantDonne.setForeground(Color.WHITE);
+		labelMontantDonne.setFont(new Font("Dialog", Font.BOLD, 12));
+		labelMontantDonne.setBounds(17, 196, 115, 14);
+		panelFacture.add(labelMontantDonne);
+		
+		labelMontantRemis = new JLabel("Montant remis:");
+		labelMontantRemis.setForeground(Color.WHITE);
+		labelMontantRemis.setFont(new Font("Dialog", Font.BOLD, 12));
+		labelMontantRemis.setBounds(17, 230, 115, 14);
+		panelFacture.add(labelMontantRemis);
+		
+		tfMontantDonne = new JTextField();
+		tfMontantDonne.setColumns(10);
+		tfMontantDonne.setBounds(220, 190, 99, 26);
+		panelFacture.add(tfMontantDonne);
+		
+		tfMontantRemis = new JTextField();
+		tfMontantRemis.setColumns(10);
+		tfMontantRemis.setBounds(220, 224, 99, 26);
+		panelFacture.add(tfMontantRemis);
+		
+		btnAnnuNouvComm = new JButton("<html>\n<p>Annuler Commande /</p>\n<p>Nouvelle Commande</p>\n</html>");
+		btnAnnuNouvComm.setFont(new Font("Lucida Grande", Font.PLAIN, 8));
+		btnAnnuNouvComm.setBounds(322, 202, 133, 48);
+		panelFacture.add(btnAnnuNouvComm);
+		
+		modele.addColumn("Produit");
+		modele.addColumn("Quantité");
+		modele.addColumn("Prix");
 
 	}
 }
